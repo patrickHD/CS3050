b5d8c1335c33fbeddcd8ebe8a36695145b58acd7
Patrick Daniel Jr <phd6x2@mail.missouri.edu>
2016-10-08 19:36:59 -0500

    Added HW1

---
+++ HW1/parser.h
@@ -0,0 +1,11 @@
+#ifndef PARSER_H_INCLUDED
+#define PARSER_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "parser.h"
+
+int parse(char* test);
+int numcheck(char* cNum);
+
+#endif // PARSER_H_INCLUDED
